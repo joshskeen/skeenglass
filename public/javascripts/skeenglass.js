9a1cffffabda149965268c6ee89d125097a788d6
josh skeen <josh@joshskeen.com>
2011-06-06 09:06:19 -0400

    add/remove to cart

--- public/javascripts/skeenglass.js
+++ public/javascripts/skeenglass.js
@@ -25,18 +25,43 @@ $(function() {
         $(this).siblings('.product_images').find('a').colorbox({open: true});
         $('<a href="/#/add_to_cart/'+product_id+'" id="add_to_cart" class="'+product_id+'" style="position:absolute; top:0;right:0px;cursor:pointer; background:#8cb3a0; color:#fff;padding:5px;">Add to cart</a>').appendTo('#cboxContent'); 
     });
+     $('.product_thumb_checkout').live('click', function(){
+         $("#add_to_cart").remove();
+        $(this).parent().parent().find('.product_images').find('a').colorbox({open: true});
+    });
 
 
     app = $.sammy(function() {
         this.element_selector = "body";
-    
-    
-        this.get('#/cart', function(context){
+        this.get('#/checkout', function(context){
             $("nav a").removeClass('active');
            $("#scrollbar_area .inner").hide();
            $("#pager").hide();
-           
+           checkout();
         });
+    
+        this.get('#/remove/:product_id', function(context){
+           var product_id = this.params['product_id'];
+           
+           var cart_items = getCartItems();
+           var idx = cart_items.indexOf(product_id);
+           if(idx != -1 && cart_items.length > 1){
+             cart_items.splice(idx, 1);
+             store.set('cart_items', cart_items.join(','));
+           }else if(cart_items.length == 1 && cart_items[0] == product_id){
+               console.log('clear all');
+               store.clearAll();
+           } 
+            $.notifyBar({
+                      cls: "success",
+                      html: "Item removed from cart.",
+                      delay: 1000,
+                      animationSpeed: "normal"
+            });
+            getCartItemsTotal();
+            context.redirect('#/checkout');
+           }
+        );
     
     
         this.get('#/add_to_cart/:product_id', function(context){
@@ -61,7 +86,7 @@ $(function() {
     
     
         this.get('#/:category_name/:page', function(context){
-            $("#scrollbar_area .inner").show();
+            //$("#scrollbar_area .inner").show();
             store = new Sammy.Store({name: 'skeenglass_cart', type: 'cookie'});
             $("#products_grid_container").empty();
             displayLoader();
@@ -125,6 +150,43 @@ function getCartItems(){
         }
     }
     return items_in_cart;
+}
+
+
+function checkout(){
+    var cart_template = '';
+    cart_item_ids = getCartItems().join(',');
+    if(cart_item_ids.length > 0){
+         $.get('/javascripts/templates/productImagesTemplate.htm', function(template) {
+            $.template("productImagesTemplate",template);    
+         });
+         $.get('/javascripts/templates/productCartTemplate.html', function(template) { 
+          cart_template = template;
+         });
+          $.get('/javascripts/templates/productTemplateTable.html', function(template) {
+          $.template("productTemplateTable",template);  
+         });
+         
+        $.ajax({
+            url: "/products/cart_items/" + cart_item_ids,
+            success: function(response){
+                item_count = 0;
+                price_total = 0;
+                $(response).each(function(){
+                    item_count++;
+                    price_total += this.price;
+                    
+                });
+                $("#products_grid_container").html(cart_template);
+                $("#cart_template .products table").html("<tr><th></th><th>Product</th><th>Price</th></tr>");
+                $("#cart_template .products table").append($.tmpl( "productTemplateTable", response));
+                $(".subtotal_price").html(price_total + "$");
+                
+            }
+        }); 
+    }else{
+        $("#products_grid_container").html("<p>No items in cart</p>");
+    }
 }
 
 function getCartItemsTotal(){
